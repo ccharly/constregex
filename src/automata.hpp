1f36fe5f3a0a7228a5c72f59ed0e7d9e8045f171
Charly Chevalier <charly.n.chevalier@gmail.com>
2015-03-01 12:12:09 +0100

    Add create_edge,get_edges_for + Rename push_state + Add final state for Automata

--- src/automata.hpp
+++ src/automata.hpp
@@ -1,19 +1,21 @@
 #ifndef AUTOMATA_HPP__
 #define AUTOMATA_HPP__
 
+#include "edge.hpp"
 #include "state.hpp"
 
-template <typename Tstates, size_t Tstart = 0>
+template <typename Tstates = mseq::empty, size_t Tstart = 0, typename Tedges = mseq::empty>
 struct Automata {
 
+    typedef Tedges edges;
     typedef Tstates states;
     static const size_t start = Tstart;
 
-    template <typename T>
-    struct push_state {
+    template <size_t Tn, bool Tfinal = false>
+    struct create_state {
         typedef
             Automata<
-                typename mseq::push<states, T>::type,
+                typename mseq::push<states, State<Tn, Tfinal> >::type,
                 start
             > type;
     };
@@ -34,6 +36,40 @@ struct Automata {
                     mseq::some<states, functor>::value,
                     NewAutomata
                 >::type type;
+    };
+
+    template <size_t Tid>
+    struct get_state {
+        private:
+            template <typename T>
+            using functor = typename id_is<Tid>::template functor<T>;
+
+        public:
+            typedef
+                typename mseq::find<states, functor>::type
+                type;
+    };
+
+    template <size_t Tlhs, size_t Trhs, char Tc>
+    struct create_edge {
+        typedef
+            Automata<
+                states,
+                start,
+                typename mseq::push<edges, Edge<Tlhs, Trhs, Tc> >::type
+            > type;
+    };
+
+    template <size_t Tid>
+    struct get_edges_for {
+        private:
+            template <typename T>
+            using functor = typename lhs_is<Tid>::template functor<T>;
+
+        public:
+            typedef
+                typename mseq::filter<edges, functor>::type
+                type;
     };
 
 };
