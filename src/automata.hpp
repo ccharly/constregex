55b56c40f5921740e4d713c36bdd1d30014021d1
Charly Chevalier <charly.n.chevalier@gmail.com>
2015-03-01 17:19:23 +0100

    Add Automata::match helper

--- src/automata.hpp
+++ src/automata.hpp
@@ -3,6 +3,7 @@
 
 #include "edge.hpp"
 #include "state.hpp"
+#include "mstring.hpp"
 
 template <typename Tstates = mseq::empty, size_t Tstart = 0, typename Tedges = mseq::empty>
 struct Automata {
@@ -70,6 +71,98 @@ struct Automata {
             typedef
                 typename mseq::filter<edges, functor>::type
                 type;
+    };
+
+    template <char Tc>
+    struct get_next_state {
+
+        template <typename T>
+        struct functor {
+            enum {
+                value = T::c == Tc
+            };
+        };
+
+        enum {
+            next =
+                mseq::find<
+                    typename get_edges_for<start>::type,
+                    functor
+                >::type::rhs
+        };
+
+        typedef typename get_state<next>::type type;
+    };
+
+    template <char Tc>
+    struct has_next_state {
+
+        template <typename T>
+        struct functor {
+            enum {
+                value = T::c == Tc
+            };
+        };
+
+        enum {
+            value =
+                mseq::some<
+                    typename get_edges_for<start>::type,
+                    functor
+                >::value
+        };
+    };
+
+    template <typename Ts, size_t Ti = 0>
+    struct match {
+
+        enum {
+            current = Ts::template at<Ti>::value
+        };
+
+        template <bool Tcond, typename Tdummy = void>
+        struct is_nextable {
+            typedef
+                typename set_start_state<
+                    get_next_state<current>::type::id
+                >::type
+                NewAutomata;
+
+            enum {
+                value =
+                    NewAutomata
+                    ::template match<Ts, Ti + 1>::value
+            };
+        };
+
+        template <typename Tdummy>
+        struct is_nextable<false, Tdummy> {
+            enum {
+                value = false
+            };
+        };
+
+        template <char Tzerobyte, typename Tdummy = void>
+        struct is_zerobyte {
+            enum {
+                value =
+                    is_nextable<
+                        has_next_state<current>::value
+                    >::value
+            };
+        };
+
+        template <typename Tdummy>
+        struct is_zerobyte<'\0', Tdummy> {
+            enum {
+                value = get_state<start>::type::is_final
+            };
+        };
+
+        enum {
+            value = is_zerobyte<current>::value
+        };
+
     };
 
 };
